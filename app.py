c504ff6ac55a3e7d8693986292c91d047a77b9bc
SPEED0806E <arpitashinde0806@gmail.com>
2026-08-06 12:44:20 +0530

    added api key and change index and intro

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request     # bring the Flask tool into my program   # request -> flask, requests -> API
-import json
 import sqlite3
 import requests
 
-
+print('New flask')
 
 app = Flask(__name__) # creating a flask application 'create my website' - turn this file in flask application and save it in a variable called app
 
@@ -24,23 +23,12 @@ history = []
 
 @app.route("/", methods=["GET","POST"]) #/ represents the homepage (http://127.0.0.1.5000/)
 def home():
-
-    
-    # meaning = ""
-
-    # if request.method == "POST":
-    #     user_input = request.form["word"].strip().lower() # gets the value entered by the user in the form field named "word", and stores in the variable user_input
-
-    #     with open("02_dictionary.json", "r") as file:
-    #         word = json.load(file)
-    #         meaning = word.get(user_input, "word not found")
-    #         history.append({"word":user_input,
-    #                         "meaning":meaning})
     
     return render_template("index.html", history=history)
 
 @app.route("/intro", methods=["GET","POST"])
 def intro():
+
 
     meaning = ""
     
@@ -55,16 +43,14 @@ def intro():
         print(response.json())
 
         data = response.json()
-        meaning = data[0]["meanings"][0]["definitions"][0]["definition"][0]
+        meaning = data[0]["meanings"][0]["definitions"][0]["definition"]
 
-        print(meaning)
+        print(f"{user_input} :", meaning)
     
-        # with open("02_dictionary.json", "r") as file:
-        #         word = json.load(file)
-        #         meaning = word.get(user_input, "word not found")
         history.append({
              "word":user_input,
              "meaning":meaning})
+        print(history)
 
         cursor.execute("""
         INSERT INTO meanings (word, meaning)
@@ -78,7 +64,7 @@ def intro():
         
         
 
-    return render_template("intro.html", history=history, meaning=meaning)
+    return render_template("intro.html", history=history, meaning="hello from flask")
 
 
 
